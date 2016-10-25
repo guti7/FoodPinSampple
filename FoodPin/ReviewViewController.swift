b39ef0a011e9ee9eb6ba852d9249ffd43b12215c
José Gutiérrez <guti7@users.noreply.github.com>
2016-10-25 00:51:04 -0700

    attach background blur effect to review view
    additionally, round review buttons

---
+++ FoodPin/ReviewViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ReviewViewController.swift
+//  FoodPin
+//
+//  Created by Guti on 10/25/16.
+//  Copyright © 2016 PielDeJaguar. All rights reserved.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
